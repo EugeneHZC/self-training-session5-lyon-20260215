a8cad4d8fac3db100ec330d49a9b5abbdc8aade5
EugeneHZC <momoeugenehan@gmail.com>
2026-02-25 13:46:48 +0800

    Added add, edit and delete functions for promotions

--- Session5_PromotionsApp/Session5_PromotionsApp/PromotionManagementPage.Designer.cs
+++ Session5_PromotionsApp/Session5_PromotionsApp/PromotionManagementPage.Designer.cs
@@ -42,8 +42,34 @@
             DeleteAction = new DataGridViewLinkColumn();
             promotionBindingSource = new BindingSource(components);
             button2 = new Button();
+            panel2 = new Panel();
+            button4 = new Button();
+            button3 = new Button();
+            numericUpDown3 = new NumericUpDown();
+            promotionBindingSource1 = new BindingSource(components);
+            label10 = new Label();
+            numericUpDown2 = new NumericUpDown();
+            label9 = new Label();
+            dateTimePicker2 = new DateTimePicker();
+            label8 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label7 = new Label();
+            listBox1 = new ListBox();
+            label6 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            label5 = new Label();
+            comboBox1 = new ComboBox();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)promotionBindingSource).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)promotionBindingSource1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             SuspendLayout();
             // 
             // dataGridView1
@@ -167,20 +193,228 @@
             button2.TabIndex = 3;
             button2.Text = "Add New Promotion";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(button4);
+            panel2.Controls.Add(button3);
+            panel2.Controls.Add(numericUpDown3);
+            panel2.Controls.Add(label10);
+            panel2.Controls.Add(numericUpDown2);
+            panel2.Controls.Add(label9);
+            panel2.Controls.Add(dateTimePicker2);
+            panel2.Controls.Add(label8);
+            panel2.Controls.Add(dateTimePicker1);
+            panel2.Controls.Add(label7);
+            panel2.Controls.Add(listBox1);
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(numericUpDown1);
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(comboBox1);
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(label2);
+            panel2.Location = new Point(41, 449);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(775, 587);
+            panel2.TabIndex = 4;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(136, 519);
+            button4.Name = "button4";
+            button4.Size = new Size(82, 37);
+            button4.TabIndex = 19;
+            button4.Text = "Cancel";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(20, 519);
+            button3.Name = "button3";
+            button3.Size = new Size(82, 37);
+            button3.TabIndex = 18;
+            button3.Text = "Save";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // numericUpDown3
+            // 
+            numericUpDown3.DataBindings.Add(new Binding("Value", promotionBindingSource1, "Priority", true));
+            numericUpDown3.Location = new Point(193, 465);
+            numericUpDown3.Maximum = new decimal(new int[] { 100000000, 0, 0, 0 });
+            numericUpDown3.Name = "numericUpDown3";
+            numericUpDown3.Size = new Size(253, 27);
+            numericUpDown3.TabIndex = 17;
+            // 
+            // promotionBindingSource1
+            // 
+            promotionBindingSource1.DataSource = typeof(models.Promotion);
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(20, 467);
+            label10.Name = "label10";
+            label10.Size = new Size(59, 20);
+            label10.TabIndex = 16;
+            label10.Text = "Priority:";
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.DataBindings.Add(new Binding("Value", promotionBindingSource1, "MinimumOrderValue", true));
+            numericUpDown2.Location = new Point(193, 420);
+            numericUpDown2.Maximum = new decimal(new int[] { 100000000, 0, 0, 0 });
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(253, 27);
+            numericUpDown2.TabIndex = 15;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(20, 422);
+            label9.Name = "label9";
+            label9.Size = new Size(149, 20);
+            label9.TabIndex = 14;
+            label9.Text = "Minimm Order Value:";
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(193, 375);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(253, 27);
+            dateTimePicker2.TabIndex = 13;
+            dateTimePicker2.Value = new DateTime(2026, 2, 9, 0, 0, 0, 0);
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(20, 380);
+            label8.Name = "label8";
+            label8.Size = new Size(73, 20);
+            label8.TabIndex = 12;
+            label8.Text = "End Date:";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(193, 330);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(253, 27);
+            dateTimePicker1.TabIndex = 11;
+            dateTimePicker1.Value = new DateTime(2026, 2, 9, 0, 0, 0, 0);
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(20, 335);
+            label7.Name = "label7";
+            label7.Size = new Size(79, 20);
+            label7.TabIndex = 10;
+            label7.Text = "Start Date:";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(193, 223);
+            listBox1.Name = "listBox1";
+            listBox1.SelectionMode = SelectionMode.MultiExtended;
+            listBox1.Size = new Size(253, 84);
+            listBox1.TabIndex = 9;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(20, 287);
+            label6.Name = "label6";
+            label6.Size = new Size(145, 20);
+            label6.TabIndex = 8;
+            label6.Text = "Applicable Products:";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.DataBindings.Add(new Binding("Value", promotionBindingSource1, "DiscountValue", true));
+            numericUpDown1.Location = new Point(193, 168);
+            numericUpDown1.Maximum = new decimal(new int[] { 100000000, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(253, 27);
+            numericUpDown1.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(20, 170);
+            label5.Name = "label5";
+            label5.Size = new Size(110, 20);
+            label5.TabIndex = 5;
+            label5.Text = "Discount Value:";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(193, 117);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(253, 28);
+            comboBox1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            textBox1.DataBindings.Add(new Binding("Text", promotionBindingSource1, "PromotionName", true));
+            textBox1.Location = new Point(193, 70);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(253, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(20, 120);
+            label4.Name = "label4";
+            label4.Size = new Size(105, 20);
+            label4.TabIndex = 2;
+            label4.Text = "Discount Type:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(20, 73);
+            label3.Name = "label3";
+            label3.Size = new Size(126, 20);
+            label3.TabIndex = 1;
+            label3.Text = "Promotion Name:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label2.Location = new Point(20, 21);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 28);
+            label2.TabIndex = 0;
+            label2.Text = "label2";
             // 
             // PromotionManagementPage
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(872, 478);
+            ClientSize = new Size(872, 1063);
+            Controls.Add(panel2);
             Controls.Add(button2);
             Controls.Add(dataGridView1);
             Name = "PromotionManagementPage";
             Text = "Belle Croissant Lyonnais - Promotion Management";
             Controls.SetChildIndex(dataGridView1, 0);
             Controls.SetChildIndex(button2, 0);
+            Controls.SetChildIndex(panel2, 0);
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ((System.ComponentModel.ISupportInitialize)promotionBindingSource).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)promotionBindingSource1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
             ResumeLayout(false);
         }
 
@@ -199,5 +433,26 @@
         private DataGridViewLinkColumn Actions;
         private DataGridViewLinkColumn DeleteAction;
         private BindingSource promotionBindingSource;
+        private Panel panel2;
+        private Button button4;
+        private Button button3;
+        private NumericUpDown numericUpDown3;
+        private Label label10;
+        private NumericUpDown numericUpDown2;
+        private Label label9;
+        private DateTimePicker dateTimePicker2;
+        private Label label8;
+        private DateTimePicker dateTimePicker1;
+        private Label label7;
+        private ListBox listBox1;
+        private Label label6;
+        private NumericUpDown numericUpDown1;
+        private Label label5;
+        private ComboBox comboBox1;
+        private TextBox textBox1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private BindingSource promotionBindingSource1;
     }
 }
--- Session5_PromotionsApp/Session5_PromotionsApp/PromotionManagementPage.cs
+++ Session5_PromotionsApp/Session5_PromotionsApp/PromotionManagementPage.cs
@@ -5,16 +5,33 @@ namespace Session5_PromotionsApp
     public partial class PromotionManagementPage : Form1
     {
         private List<Promotion> promotions = new List<Promotion>();
+        private List<Product> products = new List<Product>();
+        private string currentOperation = "";
 
         public PromotionManagementPage()
         {
             InitializeComponent();
             LoadData();
+
+            comboBox1.Items.Clear();
+            comboBox1.Items.Add("");
+            comboBox1.Items.Add("Percentage");
+            comboBox1.Items.Add("Fixed Amount");
+
+            LoadProducts();
+        }
+
+        private void LoadProducts()
+        {
+            products = Helper.db.Products.ToList();
+            listBox1.DataSource = products.Select(x => x.ProductName).ToList();
         }
 
         private void LoadData()
         {
             promotions = Helper.db.Promotions.ToList();
+
+            panel2.Hide();
 
             LoadTable();
         }
@@ -24,9 +41,52 @@ namespace Session5_PromotionsApp
             dataGridView1.DataSource = promotions;
         }
 
+        private void ResetForm()
+        {
+            listBox1.ClearSelected();
+            dateTimePicker1.Value = DateTime.Now;
+            dateTimePicker2.Value = DateTime.Now;
+        }
+
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            ResetForm();
 
+            if (e.RowIndex < 0) return;
+
+            var promotionSelected = promotions[e.RowIndex];
+
+            if (e.ColumnIndex == 8)
+            {
+                panel2.Show();
+                label2.Text = "Edit Promotion";
+
+                promotionBindingSource1.ResetCurrentItem();
+                promotionBindingSource1.DataSource = promotionSelected;
+                dateTimePicker1.Value = promotionSelected.StartDate.ToDateTime(TimeOnly.MinValue);
+                dateTimePicker2.Value = promotionSelected.EndDate.ToDateTime(TimeOnly.MinValue);
+                comboBox1.SelectedItem = promotionSelected.DiscountType == "FixedAmount" ? "Fixed Amount" : "Percentage";
+                currentOperation = "edit";
+
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                {
+                    var item = listBox1.Items[i].ToString();
+                    if (promotionSelected.ApplicableProducts.Split(",").Contains(item))
+                    {
+                        listBox1.SelectedItems.Add(item);
+                    }
+                }
+            }
+            else if (e.ColumnIndex == 9)
+            {
+                if (MessageBox.Show("Are you sure you want to delete this promotion?", "Delete Confirmation", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                {
+                    Helper.db.Promotions.Remove(promotionSelected);
+                    Helper.db.SaveChanges();
+                    LoadData();
+                    dataGridView1.ClearSelection();
+                }
+            }
         }
 
         private void dataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
@@ -39,6 +99,59 @@ namespace Session5_PromotionsApp
             {
                 e.Value = "Delete";
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ResetForm();
+
+            var newPromotion = new Promotion()
+            {
+                StartDate = DateOnly.FromDateTime(DateTime.Now),
+                EndDate = DateOnly.FromDateTime(DateTime.Now),
+                MinimumOrderValue = 0
+            };
+
+            promotionBindingSource1.ResetCurrentItem();
+            promotionBindingSource1.DataSource = newPromotion;
+            comboBox1.SelectedIndex = 0;
+            panel2.Show();
+            label2.Text = "Add New Promotion";
+            currentOperation = "add";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var promotion = (Promotion)promotionBindingSource1.Current;
+
+            promotion.DiscountType = comboBox1.SelectedItem.ToString();
+
+            promotion.ApplicableProducts = string.Join(",", listBox1.SelectedItems.Cast<string>());
+
+            promotion.StartDate = DateOnly.FromDateTime(dateTimePicker1.Value);
+            promotion.EndDate = DateOnly.FromDateTime(dateTimePicker2.Value);
+
+            if (promotion.DiscountType == "Fixed Amount")
+            {
+                promotion.DiscountType = "FixedAmount";
+            }
+
+            if (currentOperation == "add")
+            {
+                Helper.db.Add(promotion);
+            }
+            else if (currentOperation == "edit")
+            {
+                Helper.db.Entry(promotion).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            }
+            else
+            {
+                return;
+            }
+
+            Helper.db.SaveChanges();
+
+            LoadData();
         }
     }
 }
